1901a6c6aa04adb8c07198b44024e9c26aa07cd6
SquallStrifeX <squallstrifex@gmail.com>
2023-11-21 15:52:29 +0100

    end base

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 let km = prompt("Quanti km devi percorrere?");
 console.log(km);
 
-let eta = prompt("QUal è la tua età?");
+let eta = prompt("Qual è la tua età?");
 console.log(eta);
 
 let prezzo = km * 0.21;
 console.log(prezzo);
+
+if(eta < 18){
+    prezzo_finale = prezzo * 0.8
+}
+
+else if(eta > 65){
+    prezzo_finale = prezzo * 0.6
+}
+
+else{
+    prezzo_finale = prezzo
+}
+
+console.log(prezzo_finale);
+
