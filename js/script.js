38daecd053645cf3a3e9f694287d2b9e7e48960b
SquallStrifeX <squallstrifex@gmail.com>
2023-11-21 17:21:19 +0100

    end exercise

--- js/script.js
+++ js/script.js
@@ -4,23 +4,14 @@
 // let eta = prompt("Qual è la tua età?");
 // console.log(eta);
 
-let km;
-
-let eta;
-
 const button = document.querySelector(".btn");
 
 button.addEventListener("click", function(){
 
-km = document.getElementById("km").value;
-
-
-eta = document.getElementById("eta").value;
-
-} )
-
+let km = document.getElementById("km").value;
 console.log(km);
 
+let eta = document.getElementById("eta").value;
 console.log(eta);
 
 let prezzo = km * 0.21;
@@ -38,7 +29,14 @@ else{
     prezzo_finale = prezzo
 }
 
-document.getElementById("prezzo_finale").innerText = prezzo_finale
+document.getElementById("prezzo_finale").innerText = prezzo_finale.toFixed(2) + "€"
 
 console.log(prezzo_finale);
+
+
+} )
+
+
+
+
 
